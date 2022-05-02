2515216b77d386610454b01436e502e3ab582111
parshuram1saini <parshuramsaini122508@gmail.com>
2022-05-02 18:34:16 +0530

    first commit

---
+++ src/component/Stopwatch.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+function Stopwatch() {
+  const [counter, setCounter] = useState(0);
+  const [stop, setStop] = useState(false);
+
+  // stop and start button ........
+  const handlestop = () => {
+    setStop(!stop);
+    // setStop(false);
+  };
+
+  // functionality of reset button........
+
+  const handlreset = () => {
+    setCounter(0);
+    setStop(false);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (stop === false) {
+      interval = setTimeout(() => {
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    } else {
+      clearTimeout(interval);
+    }
+    return () => clearTimeout(interval); //it's for component  did unmount
+  });
+
+  return (
+    <>
+      <div className="button-frame">
+        <button className="ui labeled icon button" onClick={handlestop}>
+          <i className="pause icon"></i>
+          {stop ? "Resume" : "Stop"}
+        </button>
+        <h1>{counter}</h1>
+        <button className="ui  labeled icon button" onClick={handlreset}>
+          <i className="left arrow icon"></i>
+          Reset
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Stopwatch;
